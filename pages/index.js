7e6505e20bb1ff4043b50d58f141b8ec00910317
mayowa dabiri <mayowad43@gmai.com>
2021-02-05 11:40:44 +0100

    project initialized with nextjs and context API

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dev Story</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     
+     
+    </div>
+  )
+}
